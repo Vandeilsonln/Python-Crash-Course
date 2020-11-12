b1478b4df7b3d8ae4be677d1f6a6c0a49563718e
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-11 22:40:10 -0300

    Update dog.py

--- Chapter-9_Classes/dog.py
+++ Chapter-9_Classes/dog.py
@@ -1,25 +1,34 @@
-class Dog():
-    """A simple attempt to model a dog."""
+class Car():
+    """A simple attempt to represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
 
-    def __init__(self, name, age):
-        """Initialize name and age attributes."""
-        self.name = name
-        self.age = age
-
-    def sit(self):
-        """Simulate a dog sitting in response to a command."""
-        print(self.name.title() + " is now sitting.")
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
     
-    def roll_over(self):
-        """Simulate rolling over in responde to a command."""
-        print(self.name.title() + ' rooled over!')
-
+    def read_odometer(self):
+        print('This car has ' + str(self.odometer_reading) + ' miles on it.')
     
-my_dog = Dog('Willie', 6)
-your_dog = Dog('Lucy', 3)
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print('You can not roll back an odometer!')
 
-print("My dog's name is " + my_dog.name.title() + '.')
-print("Your dog is " + str(your_dog.age) + ' years old.')
 
-my_dog.sit()
-my_dog.roll_over()
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+my_new_car.odometer_reading = 23
+
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(5)
+my_new_car.read_odometer()
