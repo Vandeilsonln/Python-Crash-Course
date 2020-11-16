482e5f227931c37f9935b646bba306528a74b0b1
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-15 23:33:38 -0300

    show ship

--- Project_1-Alien_Invasion/alien_invasion.py
+++ Project_1-Alien_Invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 #! python3
 import sys, pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -9,6 +10,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(screen)
 
     # Start the main loop for the game.
 
@@ -20,7 +24,7 @@ def run_game():
         
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
-
+        ship.blitme()
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
--- Project_1-Alien_Invasion/ship.py
+++ Project_1-Alien_Invasion/ship.py
@@ -1,4 +1,9 @@
 #! python3
+import pygame
+from os import chdir
+
+
+chdir('.\\Project_1-Alien_Invasion')
 
 class Ship():
     def __init__(self, screen):
@@ -6,3 +11,14 @@ class Ship():
         self.screen = screen
 
         # Load the ship image and get its rect
+        self.image = pygame.image.load('_images\\ship.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Start each new ship at the bottom center of the screen
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+    
+    def blitme(self):
+        # Draw the ship at its current location.
+        self.screen.blit(self.image, self.rect)
