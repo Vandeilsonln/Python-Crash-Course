f162c36ff41bbf5e250a02c0e63877efa3085745
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-16 11:11:01 -0300

    update

--- Project_1-Alien_Invasion/alien_invasion.py
+++ Project_1-Alien_Invasion/alien_invasion.py
@@ -2,7 +2,7 @@
 import pygame
 from settings import Settings
 from ship import Ship
-import game_funcstions as gf
+import game_functions as gf
 
 
 def run_game():
@@ -18,8 +18,8 @@ def run_game():
     # Start the main loop for the game.
 
     while True:  
-        gf.check_events()
-        gr.update_screen(ai_settings, screen, ship)
+        gf.check_events(ship)
+        gf.update_screen(ai_settings, screen, ship)
 
 ##############################
 run_game()
