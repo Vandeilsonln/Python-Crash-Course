8c3208abae27da9d8b01ef34989e9d6b99bad978
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-15 23:13:27 -0300

    update

--- Project_1-Alien_Invasion/Chapter_12-Ship/alien_invasion.py
+++ Project_1-Alien_Invasion/Chapter_12-Ship/alien_invasion.py
@@ -1,15 +1,14 @@
 #! python3
 import sys, pygame
+from settings import Settings
 
 
 def run_game():
-    # Initialize game and create a screen object.
+    # Initialize pygame, settings and screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1080, 720))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
-    # Set the background color.
-    bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
 
@@ -20,7 +19,7 @@ def run_game():
                 sys.exit()
         
         # Redraw the screen during each pass through the loop.
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
---
+++ Project_1-Alien_Invasion/Chapter_12-Ship/settings.py
@@ -0,0 +1,10 @@
+#! python3
+
+class Settings():
+    """A class to store all setting for Aliens Invasion."""
+    def __init__(self):
+        # Initialize the game's settings.
+        # -- Screen settings -- #
+        self.screen_width = 1080
+        self.screen_height = 720
+        self.bg_color = (230, 230, 230)
