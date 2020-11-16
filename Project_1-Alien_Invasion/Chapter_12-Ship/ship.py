0a2c03aa9198f84062b0bad8b76280e05e427ac5
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-15 23:15:13 -0300

    Create ship.py

---
+++ Project_1-Alien_Invasion/Chapter_12-Ship/ship.py
@@ -0,0 +1,8 @@
+#! python3
+
+class Ship():
+    def __init__(self, screen):
+        # Initialaize the ship and set its starting position.
+        self.screen = screen
+
+        # Load the ship image and get its rect
