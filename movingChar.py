bf94c60c8bf68563fdffe070faa4bb073fc854c9
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-17 22:16:55 -0300

    Update movingChar.py

--- movingChar.py
+++ movingChar.py
@@ -15,10 +15,23 @@ pygame.display.set_caption('Moving Character')  # screen title
 gameBg = pygame.image.load('./Project_1-Alien_Invasion/exercises/images/game_bg.jpg')   # load background image
 myScreen.blit(gameBg, [0, 0])   # draw background image
 
-# Set up character
+# Set up character class
+class Character():
+    def __init__(self, screen):
+        self.myChar = pygame.image.load('./Project_1-Alien_Invasion/exercises/images/game_char.png')
+
+        # Get size of the character
+        self.char_size = self.myChar.get_rect()
+        self.screen_size = screen.get_rect()
+
+        # Set initial position
+        self.char_size.centerx = self.screen_size.centerx
+        self.char_size.centery = self.screen_size.centery
+
 
 # Place it on the screen
-
+mainChar = Character(myScreen)
+myScreen.blit(mainChar.myChar, mainChar.char_size)
 # While loop
 while True:
     pygame.display.flip()   # show the most recent screen
