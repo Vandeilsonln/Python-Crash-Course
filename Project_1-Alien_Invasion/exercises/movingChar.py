7d3a81a1a0316e7d6117b420551405f461f04856
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-18 15:21:59 -0300

    created character class

--- Project_1-Alien_Invasion/exercises/movingChar.py
+++ Project_1-Alien_Invasion/exercises/movingChar.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 
 
 # Set up pygame
@@ -9,17 +9,61 @@ myScreen = pygame.display.set_mode((1004, 626))
 pygame.display.set_caption('Moving character')
 
 # Create a background
+bg_image = pygame.image.load('./Project_1-Alien_Invasion/exercises/images/game_bg.jpg')
 
-# Create a character
+# Create a character class
+class Character():
+    def __init__(self, screen)
+    
+    self.char_image = pygame.image.load('./Project_1-Alien_Invasion/exercises/images/game_char.png')
+    self.screen = screen
+
+    # Get the character rect
+    self.char_rect = self.char_image.get_rect()
+    # Get the screen rect
+    self.screen_rect = self.screen.get_rect()
+
+    # Initialize the char image on the center of the screen
+    self.char_rect.centerx = self.screen_rect.centerx
+    self.char_rect.centery = self.screen_rect.centery
+
+    # Create flags to allow continuous movement
+    self.moving_right = False
+    self.moving_left = False
+    self.moving_up = False
+    self.moving_down = False
+
+    # Character speed
+    self.char_speed = 2
+
+
+    def update_char_position(event, char):
+        # It will set the flags to true based on which event was passed.
+        if self.moving_left and self.char_rect.left > 0:
+            self.char_rect.centerx -= self.char_speed
+        
+        if self.moving_right and self.char_rect.right < self.screen_rect.right:
+            self.char_rect.centerx += self.char_speed
+        
+        if self.moving_up and self.char_rect.up > 0:
+            self.char_rect.centery -= self.char_speed
+        
+        if self.moving_down and self.char_rect.bottom < self.screen_rect.bottom:
+            self.char_rect.centery += self.char_speed
+
+
+# Create Character
+myChar = Character(myScreen)
 
 # Function to handle events
 
 while True:
-    pass
     # Check for keyboard presses
 
     # Update the position
 
+
     # Blit background and ship
 
     # Show the most recently drawn screen
+    pygame.display.flip()
