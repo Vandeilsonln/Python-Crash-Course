0c720a7dc6dcbb21a93bd7a592e8c76e0e5c43f0
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-17 23:39:33 -0300

    ship drawn on the screen

--- Project_1-Alien_Invasion/alien_invasion.py
+++ Project_1-Alien_Invasion/alien_invasion.py
@@ -1,5 +1,7 @@
 import sys, pygame
 from settings import Settings
+from ship import Ship
+
 
 def run_game():
     # Initialize game, settings and screen object.
@@ -7,6 +9,9 @@ def run_game():
     mySettings = Settings()
     myScreen = pygame.display.set_mode((mySettings.screen_width, mySettings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+
+    # Make a ship
+    ship = Ship(myScreen)
 
     # Start the main loop for the game.
     while True:
@@ -18,6 +23,10 @@ def run_game():
         
         # Redraw the screen during each pass through the loop.
         myScreen.blit(mySettings.bg_image.convert(), [0, 0])
+
+        # Redraw the ship on the screen, with its position adjusted
+        ship.blitme()
+        
         # make the most recently drawn screen visible.
         pygame.display.flip()
 
--- Project_1-Alien_Invasion/settings.py
+++ Project_1-Alien_Invasion/settings.py
@@ -6,5 +6,5 @@ class Settings():
     def __init__(self):
         # Screen Settings.
         self.screen_width = 1080
-        self.screen_height = 720
+        self.screen_height = 620
         self.bg_image = pygame.image.load('Project_1-Alien_Invasion/_images/background_stars.jpg')
