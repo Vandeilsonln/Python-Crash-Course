dedf0516f9884b7ee28af668e11e5f585b10e287
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-19 09:46:58 -0300

    update bullet

--- Project_1-Alien_Invasion/alien_invasion.py
+++ Project_1-Alien_Invasion/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -9,19 +10,24 @@ def run_game():
     pygame.init()
     mySettings = Settings()
     
+    # Make a screen
     myScreen = pygame.display.set_mode((mySettings.screen_width, mySettings.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
     # Make a ship
     ship = Ship(mySettings, myScreen)
 
+    # Make a group to store bullets in.
+    bullets = Group()
+
     # Start the main loop for the game.
     while True:
 
         # Watch for keyboard and mouse events.
-        gf.check_events(ship)   # Handles keyboard
-        ship.update_ship_position()        
-        gf.update_screen(mySettings, myScreen, ship)    # Blit ship, background and update screen  
+        gf.check_events(ship, bullets)   # Handles keyboard
+        ship.update_ship_position()
+        bullets.update_bullet_position()        
+        gf.update_screen(mySettings, myScreen, ship, bullets)    # Blit ship, background and update screen  
 
         # make the most recently drawn screen visible.
         pygame.display.flip()
--- Project_1-Alien_Invasion/bullet.py
+++ Project_1-Alien_Invasion/bullet.py
@@ -7,11 +7,11 @@ class Bullet(Sprite):
 
     def __init__ (self, ai_settings, screen, ship):
         # Create a bullet object at the ship's current position.
-        super(Bullet, self).__init__()
+        super().__init__()
         self.screen = screen
 
         # Create a bullet rect at (0,0) and then set current position.
-        self.bullet_rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.bullet_rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height) # TODO: Replace this RECT object for an bullet image.
         self.bullet_rect.centerx = ship.rect.centerx
         self.bullet_rect.top = ship.rect.top
 
@@ -20,4 +20,17 @@ class Bullet(Sprite):
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
+
+        def update_bullet_position(self):
+            # Move the bullet up the screen
+
+            # Update the decimal position of the bullet.
+            self.y -= self.speed_factor
+
+            # Update the rect position.
+            self.bullet_rect.y = self.y
         
+        def draw_bullet(self):
+            # Draw the bullet to the screen.
+            pygame.draw.rect(self.screen, self.color, self.rect)    # TODO: use self.screen.blit() method to place an image instead of a rect.
+            
