44b8cc8e99262d57b4b1cb1bda0e98d807677289
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-20 17:49:22 -0300

    alien class created

---
+++ alien.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    '''A class to represent a single alien in the fleet.'''
+
+    def __init__(self, ai_settings, screen):
+        # Initialize the alienand set its starting position.
+        super().__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the alien image and set its rect atribute.
+        self.image = pygame.image.load('Project_1-Alien_Invasion/images/alien.png')
+        self.alien_rect = self.image.rect()
+
+        # Start each new alien near the top left of the screen.
+        self.alien_rect.x = self.alien_rect.width
+        self.alien_rect.y = self.alien_rect.height
+
+        # Store the alien's exact position.
+        self.x = float(self.alien_rect.x)
+
+    def blitme(self):
+        # Draw the alien at its current location.
+        self.screen.blit(self.image, self.alien_rect)
