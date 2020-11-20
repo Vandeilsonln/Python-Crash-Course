5d94f2505491cabe2f737eee10a75d6fa28d87ee
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-19 21:33:17 -0300

    update

--- Project_1-Alien_Invasion/settings.py
+++ Project_1-Alien_Invasion/settings.py
@@ -8,14 +8,14 @@ class Settings():
         self.screen_width = 1080
         self.screen_height = 630
         self.bg_image = pygame.image.load('Project_1-Alien_Invasion/_images/background_stars_moving.jpg')
-        self.bg_moving_speed = 5
+        self.bg_moving_speed = 0.5
         self.bg_initial_position = -1705
         
         # Ship Settings
-        self.ship_speed_factor = 2
+        self.ship_speed_factor = 2.5
 
         # Bullet settings
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 130, 60, 60
