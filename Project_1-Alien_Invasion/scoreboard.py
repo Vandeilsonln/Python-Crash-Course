a9aaf3eb5fdc24dfb9c729ec5523c846e6825cf4
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-12-18 19:48:22 -0300

    Update scoreboard.py

--- Project_1-Alien_Invasion/scoreboard.py
+++ Project_1-Alien_Invasion/scoreboard.py
@@ -17,3 +17,17 @@ class Scoreboard():
 
         # Prepare the initial score image.
         self.prep_score()
+    
+    def prep_score(self):
+        # Turn the score into a rendered image.
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True, self.text_color, (0, 0, 0))
+
+        # Display the score at the top right of the screen.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+    
+    def show_score(self):
+        # Draw score to the screen
+        self.screen.blit(self.score_image, self.score_rect)
