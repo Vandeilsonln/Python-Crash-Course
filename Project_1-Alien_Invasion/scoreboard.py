4bfd314824eb94e225f6e428c690a47ebd0c40ac
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-12-18 23:02:31 -0300

    labels added

--- Project_1-Alien_Invasion/scoreboard.py
+++ Project_1-Alien_Invasion/scoreboard.py
@@ -13,7 +13,7 @@ class Scoreboard():
 
         # Font settings for scoring information.
         self.text_color = (255, 255, 255)
-        self.font = pygame.font.SysFont(None, 36)
+        self.font = pygame.font.SysFont(None, 24)
 
         # Prepare the initial score images.
         self.prep_score()
@@ -23,7 +23,7 @@ class Scoreboard():
     def prep_high_score(self):
         # Turn the high score into a rendered image.
         high_score = int(round(self.stats.high_score, -1))
-        high_score_str = "{:,}".format(high_score)
+        high_score_str = "HIGH SCORE: " + "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True, self.text_color, (0, 0, 0))
 
         # Center the high score at the top of the screen
@@ -34,7 +34,7 @@ class Scoreboard():
     def prep_score(self):
         # Turn the score into a rendered image.
         rounded_score = int(round(self.stats.score, -1))
-        score_str = "{:,}".format(rounded_score)
+        score_str = "SCORE: " + "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color, (0, 0, 0))
 
         # Display the score at the top right of the screen.
@@ -42,17 +42,17 @@ class Scoreboard():
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
     
-    def show_score(self):
-        # Draw score and ships to the screen
-        self.screen.blit(self.score_image, self.score_rect)
-        self.screen.blit(self.high_score_image, self.high_score_rect)
-        self.screen.blit(self.level_image, self.level_rect)
-
     def prep_level(self):
         # Turn the level into a rendered image
-        self.level_image = self.font.render(str(self.stats.level), True, self.text_color, (0, 0, 0))
+        self.level_image = self.font.render("LEVEL: " + str(self.stats.level), True, self.text_color, (0, 0, 0))
 
         # Position the level below the score.
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+
+    def show_score(self):
+        # Draw score and ships to the screen
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
