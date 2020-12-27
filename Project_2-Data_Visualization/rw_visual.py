32f50ce97a709a2d17442337e5e7f8731b50c124
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-12-26 23:12:43 -0300

    random walk

---
+++ Project_2-Data_Visualization/rw_visual.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# Make a random walk, and plot the points.
+rw = RandomWalk(20000)
+rw.fill_walk()
+
+plt.scatter(rw.x_values, rw.y_values, s=1)
+plt.show()
--- ex.py
+++ ex.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
 
-x_values = list(range(1, 1001))
-y_values = [x**2 for x in x_values]
+x_values = list(range(1, 5001))
+y_values = [x**3 for x in x_values]
 plt.scatter(x_values, y_values, s=10, edgecolors='none', c=y_values, cmap=plt.cm.seismic)
 
 # Set the chart title and label axes
@@ -13,7 +13,4 @@ plt.ylabel('Square of number', fontsize=14)
 # Set size of tick labels.
 plt.tick_params(axis='both', which='major', labelsize=14)
 
-# Set the range of each axis.
-plt.axis([0, 1100, 0, 1100000])
-
-plt.savefig('./Project_2-Data_Visualization/exercises/square_plot.png', bbox_inches='tight')
+plt.show()
