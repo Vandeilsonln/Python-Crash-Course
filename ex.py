3cf52af118a4331232e7682be3301171a0a0af6c
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-12-26 22:35:37 -0300

    save plot as png

--- ex.py
+++ ex.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
-input_values = [1, 2, 3, 4, 5]
-plt.plot(input_values, squares, linewidth=5)
 
-# Set chart title and label axes.
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values, s=10, edgecolors='none', c=y_values, cmap=plt.cm.seismic)
+
+# Set the chart title and label axes
 plt.title('Square Numbers', fontsize=24)
 plt.xlabel('Value', fontsize=14)
-plt.ylabel('Square of value', fontsize=14)
+plt.ylabel('Square of number', fontsize=14)
 
 # Set size of tick labels.
-plt.tick_params(axis='both', labelsize=14)
+plt.tick_params(axis='both', which='major', labelsize=14)
 
-plt.show()
+# Set the range of each axis.
+plt.axis([0, 1100, 0, 1100000])
+
+plt.savefig('./Project_2-Data_Visualization/exercises/square_plot.png', bbox_inches='tight')
