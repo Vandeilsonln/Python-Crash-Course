b9060f8cf22d942b212c67fe8d78d6641535c2f3
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-12-29 23:15:04 -0300

    update

--- Project_2-Data_Visualization/rw_visual.py
+++ Project_2-Data_Visualization/rw_visual.py
@@ -7,7 +7,20 @@ while True:
     rw = RandomWalk(10000)
     rw.fill_walk()
 
-    plt.scatter(rw.x_values, rw.y_values, s=1)
+    # Set the size of the plotting window
+    plt.figure(figsize=(10, 6))
+
+    point_numbers = list(range(0, rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Wistia, edgecolor='none', s=1)
+
+    # Emphasize the first and last points
+    plt.scatter(0, 0, c='green', edgecolors='none', s=20)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=20)
+
+    # remove the axis
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input('Make another walk? (y/n): ')
