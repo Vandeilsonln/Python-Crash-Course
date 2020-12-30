131e3a63e025261ade2f21e4a94e29e8538359d8
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-12-29 23:47:25 -0300

    Update dice_visual.py
    testing

--- Project_2-Data_Visualization/exercises/dice_visual.py
+++ Project_2-Data_Visualization/exercises/dice_visual.py
@@ -2,11 +2,29 @@ from dice import Dice
 
 # Create a D6
 dice = Dice()
+number_rolls = 1000
 
 # Make some rolls, and store results in a list
 results = []
-for roll in range(100):
+for roll in range(number_rolls):
     result = dice.roll()
     results.append(result)
 
-print(results)
+# analyze the results
+frequencies = []
+for value in range(1, dice.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
+# test
+percent = [(i / number_rolls * 100) for i in frequencies]
+print(percent)
+
+# test 2 
+data = {}
+for i in range(1, dice.num_sides+1):
+    data[i] = percent[i-1]
+
+print(data)
