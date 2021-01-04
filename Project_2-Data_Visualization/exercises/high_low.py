96df2cae291d2bd3137118da3e2ea6f82004c6ae
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-03 23:30:47 -0300

    x labels

--- Project_2-Data_Visualization/exercises/high_low.py
+++ Project_2-Data_Visualization/exercises/high_low.py
@@ -1,6 +1,7 @@
 import csv
 from os import chdir
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 # Set up Current Working Directiory
 chdir('./Project_2-Data_Visualization/exercises')
@@ -9,25 +10,29 @@ chdir('./Project_2-Data_Visualization/exercises')
 def convertTemperature(degrees):
     return int(round((degrees - 32) * 5 / 9, 0))
 
-# Get high temperatures from file.
+# Get dates and high temperatures from file.
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
         highFarenheit = int(row[1])
         highCelsius = convertTemperature(highFarenheit)
         highs.append(highCelsius)
 
 # Plot data
-fig = plt.figure(dpi=128, figsize=(9, 6))
-plt.plot(highs, c='red')
+fig = plt.figure(dpi=128, figsize=(8, 5))
+plt.plot(dates, highs, c='red')
 
 # Format plot
 plt.title('Daily high temperatures, July 2014', fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
