acf35c8965a4df11988d5679899b678b09e08749
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-05 16:23:15 -0300

    update

--- worldpopulation.py
+++ worldpopulation.py
@@ -13,6 +13,9 @@ filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
+# Build a dictionary of population data.
+cc_populations = {}
+
 # Print the 2010 population for each country.
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
@@ -20,6 +23,10 @@ for pop_dict in pop_data:
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(code + ": " + str(population))
-        else:
-            print('Error - ' + country_name)
+            cc_populations[code] = population
+
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('./_images/world_population.svg')
