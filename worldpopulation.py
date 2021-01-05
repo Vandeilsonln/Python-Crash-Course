f61e8bc277f5b91a475f7582031e4eaefa780576
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-05 16:52:39 -0300

    updated

--- worldpopulation.py
+++ worldpopulation.py
@@ -25,8 +25,20 @@ for pop_dict in pop_data:
         if code:
             cc_populations[code] = population
 
+# Group the countries into 3 population levels.
+cc_pops1, cc_pops2, cc_pops3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops2[cc] = pop
+    else:
+        cc_pops3[cc] = pop
+
 wm = pygal.maps.world.World()
 wm.title = 'World Population in 2010, by Country'
-wm.add('2010', cc_populations)
+wm.add('0-10m', cc_pops1)
+wm.add('10m-1bn', cc_pops2)
+wm.add('>1bn', cc_pops3)
 
 wm.render_to_file('./_images/world_population.svg')
