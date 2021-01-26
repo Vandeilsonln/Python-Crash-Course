cfcc2f893dcbec680dfd88dfc02ee8dc887d1c8f
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-25 22:38:52 -0300

    Update python_repos.py

--- Project_2-Data_Visualization/exercises/python_repos.py
+++ Project_2-Data_Visualization/exercises/python_repos.py
@@ -14,8 +14,12 @@ print('Total repositories:', response_dict['total_count'])
 repo_dicts = response_dict['items']
 print('repositories returned:', len(repo_dicts))
 
-# examine the first repository
-repo_dict = repo_dicts[0]
-print('\nKeys:', len(repo_dict))
-for key in sorted(repo_dict.keys():
-    print(key))
+print('\nSelected information about each repository:')
+for repo_dict in repo_dicts:
+    print('\nName:', repo_dict['name'])
+    print('Owner':, repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Description:', repo_dict['description'])
+
+    
