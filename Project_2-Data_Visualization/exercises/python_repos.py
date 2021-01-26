39b4f421504618f947672304a8e97edf7bc7f13d
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-25 23:37:12 -0300

    update

--- Project_2-Data_Visualization/exercises/python_repos.py
+++ Project_2-Data_Visualization/exercises/python_repos.py
@@ -21,7 +21,7 @@ for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
     plot_dict = {
         'value': repo_dict['stargazers_count'],
-        'label': repo_dict['description'],
+        #'label': repo_dict['description'],
         'xlink': repo_dict['html_url'],
     }
     plot_dicts.append(plot_dict)
@@ -33,9 +33,9 @@ my_style = LS('#333366', base_style=LCS)
 my_config = pygal.Config()
 my_config.x_label_rotation = 45
 my_config.show_legend = False
-my_config.title_font_size = 24
-my_config.label_font_size = 14
-my_config.major_label_font_size = 18
+my_config.title_font_size = 34
+my_config.label_font_size = 20
+my_config.major_label_font_size = 28
 my_config.truncate_label = 15
 my_config.show_y_guides = False
 my_config.width = 1000
