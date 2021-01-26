17266ef9dc0563dfcdee10f67a434f5b1d80252d
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-25 23:21:53 -0300

    Update python_repos.py

--- Project_2-Data_Visualization/exercises/python_repos.py
+++ Project_2-Data_Visualization/exercises/python_repos.py
@@ -15,10 +15,17 @@ print('Total repositories:', response_dict['total_count'])
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
+    
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
@@ -37,5 +44,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('./Project_2-Data_Visualization/exercises/_images/python_repos.svg')
